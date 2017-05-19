31ae72e5638d7b1310bb3402ae6dd7235dd99763
Brendan White <SKYNET@Brendans-MacBook.local>
2017-05-19 16:12:31 -0500

    comments can be deleted

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,4 +6,11 @@ class CommentsController < ApplicationController
 
     redirect_to recipe_path(@recipe)
   end
+
+  def destroy
+    @recipe = Recipe.find(params[:recipe_id])
+    @comment = @recipe.comments.find(params[:id])
+    @comment.destroy
+    redirect_to recipe_path(@recipe)
+  end
 end
