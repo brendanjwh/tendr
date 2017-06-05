8c8bda6e2d61127ac49913544ac9b57844173152
Brendan White <eggrollcontrol@gmail.com>
2017-06-05 16:36:34 -0500

    more syntax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,8 @@
 class FavoritesController < ApplicationController
   
   def create
-    if Favorite.create(user_id: current_user, recipe_id: )
-    @favorite = Favorite.new
+    # if Favorite.create(user_id: current_user, recipe_id: )
+    # @favorite = Favorite.new
   end
 
 end
